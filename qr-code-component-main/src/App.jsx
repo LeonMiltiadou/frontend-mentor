7be9f37981ac5d8d105a192b74ce6ae4ec1a47fd
Leon Miltiadou <leon.miltiadou@hotmail.co.uk>
2023-02-05 18:26:05 +0000

    finished qr-code-component

---
+++ qr-code-component-main/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+import QR from "./components/qr/qr.jsx";
+
+function App() {
+  return (
+    <div className="App">
+      <QR></QR>
+    </div>
+  );
+}
+
+export default App;
---
+++ qr-code-component-main/src/components/qr/qr.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import "./index.css";
+import qrImage from "../../../images/image-qr-code.png";
+
+function QR() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="qr-container">
+      <img className="qr-code" src={qrImage} alr="qr image" />
+      <h1 class="qr-title">
+        Improve your front-end skills by building projects
+      </h1>
+      <p class="qr-paragraph">
+        Scan the QR code to visit Frontend Mentor and take your coding skills to
+        the next level
+      </p>
+    </div>
+  );
+}
+
+export default QR;
